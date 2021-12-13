00b875c0daacd2763411d2c8a468d13e3e63c205
Eden Skånberg <eden.89@live.com>
2021-12-13 12:10:05 +0100

    readmore

--- blog-client-template/js/posts.js
+++ blog-client-template/js/posts.js
@@ -22,50 +22,25 @@ async function OurBlogContent() {
       `;
     }
     document.getElementById('blog-list').innerHTML = contentHTML;
+    let displayCharacter = 100;
+    const allArticles = document.getElementsByClassName('article');
+    // console.log(allArticles);
+    let articlesArray = [...allArticles];
+    // console.log(arr);
 
-  } catch (error) {
-        console.log(error);
+    articlesArray.forEach(article => {
+      if (article.innerHTML.length < displayCharacter) {
+        article.nextElementSibling.style.display = 'none';
+      } else {
+        const display100Char = article.innerHTML.slice(0, displayCharacter);
+        article.innerHTML = display100Char;
       }
+    });
+    } catch (error) {
+    console.log(error);
   }
-
-
-
-//     let displayCharacter = 100;
-//     const allArticles = document.getElementsByClassName('article');
-//     // console.log(allArticles);
-//     let articlesArray = [...allArticles];
-//     // console.log(arr);
-
-//     articlesArray.forEach(article => {
-//       if (article.innerHTML.length < displayCharacter) {
-//         article.nextElementSibling.style.display = 'none';
-//       } else {
-//         const display100Char = article.innerHTML.slice(0, displayCharacter);
-//         article.innerHTML = display100Char;
-//       }
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// displayOurBlogContent();
-
-
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      moreText.style.display = "inline";
-    }
-  }
-
+}
 
 OurBlogContent();
-  
+
+
